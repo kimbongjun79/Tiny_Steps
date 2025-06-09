66d4a95fbc6aeae872ae8de6efc09bbbdf041d06
kimyoungchae <acusea@naver.com>
2025-06-09 15:36:50 +0900

    Stage3 씬, 버튼, 리소스 main 기반 최종 반영

---
+++ texture_button_4.gd
@@ -0,0 +1,4 @@
+extends TextureButton
+
+func _on_texture_button4_pressed():
+	get_tree().change_scene_to_file("res://Stages/Stage4.tscn")
---
+++ texture_button_5.gd
@@ -0,0 +1,4 @@
+extends TextureButton
+
+func _on_texture_button5_pressed():
+	get_tree().change_scene_to_file("res://Stages/Stage5.tscn")
